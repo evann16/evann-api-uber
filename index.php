1f86dc967fe5adb33cc7a91dd35262f7481c945d
evann16 <evannfa@gmail.com>
2025-10-08 17:18:59 +0100

    Modification du index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $voitureController = new VoitureController();
 // Vérifie si le paramètre "page" est vide ou non présent dans l'URL
 if (empty($_GET["page"])) {
     // Si le paramètre est vide, on affiche un message d'erreur
-    echo "La page n'existe pas";
+    echo "Cette page est introuvable.";
 } else {
     // Sinon, on récupère la valeur du paramètre "page"
     // et on la découpe en plusieurs morceaux, séparés par "/"
